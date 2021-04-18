ec71eb8d9b3fd4af7dd3ca1664330746fc31216e
Donovan McGuire <donovanjmcguire@gmail.com>
2021-04-18 10:58:33 -0500

    Update Login.js

--- forms/Login/Login.js
+++ forms/Login/Login.js
@@ -2,22 +2,42 @@ let req = ""
 let query = ""
 let results = ""
 let netID = ""
-let pw = ""
+let pw = "Bolognabaddie37!"
 let allUserData = []
 
+/*
+loginPage.onshow = function() {
+    query = "SELECT * FROM user"
+    req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user" + netID + "&pass=" + pw + "$database=" + netID + "$query=" + query)
+    if (req.status == 200) { //transit trip worked. 
+        console.log(`The results are: \n ${results}`)
+        results = JSON.parse(req.responseText)
+        //console.log(`The parsed JSON string is converted to a JS object (an array of arrays): ${results} where results[0] is ${results[0]}, the first array in the JS results object.`)
+        if (results.length == 0)
+            lblMessage.value = "There are no customers in the database."
+        else {
+            let message = ""
+            for (i = 0; i < results.length; i++)
+                message = message + results[i][1] + "\n"
+            inptNetID.value = message
+        }
+    } else // the transit didn't work - bad wifi? server turned off?
+        lblMessage.value = "Error code: " + req.status
+}
+*/
 
 btnLogin.onclick = function() {
-    let inputUsername = inptUsername.value
+    let inputUsername = inptNetID.value
     let inputPassword = inptPassword.value
-    query = `SELECT username FROM user`
-    req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=kmh76825&pass=" + pw + "&database=375groupa1&query=" + query)
+    query = `SELECT user_id FROM user`
+    req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=djm42254&pass=" + pw + "&database=375groupb5&query=" + query)
     if (req.status == 200) { //transit worked.
         console.log(req.status)
         console.log(req.responseText)
         usernames = JSON.parse(req.responseText)
         console.log(usernames)
         query = `SELECT password FROM user`
-        req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=kmh76825&pass=" + pw + "&database=375groupa1&query=" + query)
+        req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=djm42254&pass=" + pw + "&database=375groupb5&query=" + query)
         if (req.status == 200) { //transit worked.
             console.log(req.status)
             console.log(req.responseText)
@@ -33,11 +53,11 @@ btnLogin.onclick = function() {
                     }
                 }
             }
-            if (valid) {
+            if (valid = true) {
                 alert("Valid Credentials")
-                ChangeForm(homePage)
+                ChangeForm(homePage) //NO HOMEPAGE YET
                 query = `SELECT  user_id FROM user WHERE  username = "` + inputUsername + `"`
-                req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=kmh76825&pass=" + pw + "&database=375groupa1&query=" + query)
+                req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=djm42254&pass=" + pw + "&database=375groupb5&query=" + query)
                 if (req.status == 200) { //transit worked.
                     console.log(req.status)
                     console.log(req.responseText)
